58a0bd61b0a091b570b62ca9a20045e5c36dbbbe
Alberto Mardegan <alberto.mardegan@canonical.com>
2012-07-26 08:25:00 +0300

    Tests: update license
    We forgot to change the license in tests/check_signon.c when the project
    was open-sourced.

--- tests/check_signon.c
+++ tests/check_signon.c
@@ -1,7 +1,25 @@
-/**
- * Copyright (C) 2009 Nokia Corporation.
- * Contact: Alberto Mardegan <alberto.mardegan@nokia.com>
- * Licensed under the terms of Nokia EUSA (see the LICENSE file)
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of libsignon-glib
+ *
+ * Copyright (C) 2009-2011 Nokia Corporation.
+ * Copyright (C) 2011-2012 Canonical Ltd.
+ *
+ * Contact: Alberto Mardegan <alberto.mardegan@canonical.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  */
 
 /**
